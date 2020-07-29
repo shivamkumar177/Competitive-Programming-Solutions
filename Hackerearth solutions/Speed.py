dc087c749ec275c84d5486addfb55f1f6815d9c9
Madhusudhan Gupta <49074760+Madhusudhangupta@users.noreply.github.com>
2020-07-29 08:14:49 +0530

    Create Speed.py

---
+++ Hackerearth solutions/Speed.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())):
+    n = int(input())
+    l = list(map(int,input().split()))
+    c=1
+    ma = l[0]
+    
+    for i in range(1,n):
+        if l[i]<=ma:
+            c+=1
+            ma = l[i]
+    print(c)
